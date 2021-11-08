d531be270a83730edbc5aaca29ae886b28ea64ff
SaiDeepika522 <cse.saideepika9@gmail.com>
2021-11-08 19:53:57 +0530

    sai files

---
+++ main.py
@@ -0,0 +1,12 @@
+def check_palindrome(str1):
+	if str1[::-1]==srr1:
+		return True
+	else:
+		return False
+if(check_palindrome(str1)==True):
+	print("paindrome")
+else:
+	print("not palindrome")
+
+
+
